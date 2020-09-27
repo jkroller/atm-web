595f07c75584aced6f9d3fcaadb1eea789407931
Pinpong Weluwanathon <pinphong.w@ku.th>
2020-09-27 22:54:50 +0700

    add Database jdbc

---
+++ src/main/java/th/ac/ku/atm/controller/CustomerController.java
@@ -0,0 +1,32 @@
+package th.ac.ku.atm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import th.ac.ku.atm.model.Customer;
+import th.ac.ku.atm.service.CustomerService;
+
+@Controller
+public class CustomerController {
+
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/customer")
+    public String getCustomerPage(Model model) {
+        model.addAttribute("allCustomers", customerService.getCustomers());
+        return "customer";
+    }
+
+    @PostMapping("/customer")
+    public String registerCustomer(@ModelAttribute Customer customer, Model model) {
+        customerService.createCustomer(customer);
+        model.addAttribute("allCustomers", customerService.getCustomers());
+        return "redirect:customer";
+    }
+}
